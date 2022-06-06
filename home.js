230f603618cfb455eb05fab5e1d6916adc1cd3bc
Lucas H <lucas.henriquevio@gmail.com>
2022-06-06 17:51:01 -0300

    Primeiro commit

---
+++ home.js
@@ -0,0 +1,14 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+let logado = document.querySelector('#logado')
+logado.innerHTML = `Bem-vindo ${userLogado.nome}`
+
+if(localStorage.getItem('token') == null){
+    alert('Você precisa estar logado para acessar essa página')
+    window.location.href = 'login.html'
+}
+
+function sair(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('userLogado')
+    window.location.href = 'login.html'
+}
